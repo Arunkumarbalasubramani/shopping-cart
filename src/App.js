88073f91183f387a89c4968400e605e44faaa13f
Arunkumarbalasubramani <arunkumar.vbs.b@gmail.com>
2022-11-01 12:13:28 +0530

    Added Carousel page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbarcomponent from "./Navbar";
+import Carouselpage from "./carouselpage";
 function App() {
   return (
     <div className="App">
       <Navbarcomponent />
-
+      <Carouselpage />
       <h1>Welcome to Shoppincart task</h1>
     </div>
   );
---
+++ src/carouselpage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./carousel.scss";
+const Carouselpage = () => {
+  return (
+    <div className="carousel-container">
+      <h2 className="heading"> Shop in Style</h2>
+      <p className="text">With this Shop homepage template</p>
+    </div>
+  );
+};
+
+export default Carouselpage;
